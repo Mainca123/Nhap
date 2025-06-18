e481cd8cfb67d4b7f4e24b244faf945fd4e3f7b2
Mainca123 <giangchinhtgp@gmail.com>
2025-06-18 16:05:09 +0700

    Set RSA

--- RSA.cpp
+++ RSA.cpp
@@ -2,76 +2,84 @@
 using namespace std;
 #define ll long long
 
-ll modInverse(ll e, ll phi){
-    ll  t=0, newt=1,
-        r=phi, newr=e;
-    while(newr != 0){
-        ll quotient=r / newr;
-        ll temp=newt;
-        newt=t - quotient * newt;
-        t=temp;
-        temp = newr;
-        newr=r - quotient * newr;
-        r=temp;
+ll Mu(ll a, ll n, ll mod){
+    ll res = 1;
+    a = a % mod;
+    while (n > 0){
+        if (n % 2 == 1)
+            res = (res * a) % mod;
+        a = (a * a) % mod;
+        n /= 2;
     }
-    if(r>1) return -1;
-    if(t<0) t+=phi;
-    return t;
-}
-
-ll Pow(ll a, ll n, ll mod){
-    if(n==0) return 1;
-    ll tam=Pow(a, n/2, mod);
-    tam*=tam%mod;
-    tam%=mod;
-    if(n%2 !=0)
-        tam*=a%mod;
-    tam%=mod;
-    return tam;
+    return res;
 }
 
 bool nt(ll a){
-    if(a<2) return 0;
-    for(int i=2; i*i<=a; i++)
-        if(a%i==0) return 0;
-    return 1;
+	if(a<2) return 0;
+	for(int i=2; i*i<=a; i++)
+		if(a%i==0) return 0;
+	return 1;
 }
 
-ll locNguyenTo(ll a){
-    while(!nt(a))
-        a--;
-    return a;
+
+ll NghichDaoModulo(ll e, ll phi){
+	ll tCu=0, tMoi=1;
+	ll rCu=phi, rMoi=e;
+	while(rMoi!=0){
+		ll tam = tMoi;
+		tMoi = tCu - (rCu/rMoi)*tMoi;
+		tCu=tam;
+
+		tam=rMoi;
+		rMoi = rCu%rMoi;
+		rCu = tam; 
+	}
+	if(rCu == 0) return -1;
+	if(tCu<0) tCu += phi;
+	return tCu;
+
 }
-int main()
-{
-    cout<<"Nhap gioi han tren cua 2 so nguyen to\n";
-    ll a,b;
-    cout<<"Nhap gioi han 1: ", cin>>a, a=locNguyenTo(a);
-    cout<<"So nguyen to lon nhat la: "<<a<<'\n';
-    cout<<"Nhap gioi han 2: ", cin>>b, b=locNguyenTo(b);
-    cout<<"So nguyen to lon nhat la: "<<b<<'\n';
-    ll n=a * b;
-    ll phi=(a-1) * (b-1);
-    cout<<"Ta co phi = "<<phi<<'\n';
-    ll e;
-    cout<<"Nhap gioi han duoi cau ma bi mat (1 < e < phi): ", cin>>e;
-    while(__gcd(e,phi) != 1)
-        e++;
 
-    ll d=modInverse(e, phi);
-    if(d == -1){
-        cout<<"Khong tim duoc d hop le";
-        return 1;
-    }
+int main() {
+	cout<<"Nhap so nguyen to P = ";
+	ll p;
+	cin>>p;
+	while (!nt(p)){
+		cout<<"Nhap lai P = ";
+		cin>>p;
+	}
+	
+	cout<<"Nhap so nguyen to Q = ";
+	ll q;
+	cin>>q;
+	while (!nt(q)){
+		cout<<"Nhap lai Q = ";
+		cin>>q;
+	}
+	
+	ll n=p*q;
+	ll phi=(p-1)*(q-1);
 
-    cout<<"Public key: n = "<<n<<" , e = "<<e<<'\n';
-    cout<<"Private key: n = "<<n<<" , d = "<<d<<'\n';
-    ll banRo;
-    cout<<"Nhap ban ro (so nguyen < "<<n<<"): ", cin>> banRo;
+	ll e;
+	cout<<"Nhap khoa cong khai E = ", cin>>e;
+	if(__gcd(e,phi) != 1)
+		cout<<"Khoa cong khai khong hop le !!!\n";
+	while( __gcd(e,phi) != 1){
+		cout<<"Nhap lai E = ",cin>>e;
+	}
 
-    ll banMa = Pow(banRo, e, n);
-    cout<<"Ban ma: "<< banMa<<'\n';
+	ll d=NghichDaoModulo(e,phi);
 
-    ll giaiMa = Pow(banMa, d, n);
-    cout<<"Giai ma: "<< giaiMa;
+	ll banRo;
+	cout<<"Nhap ban ro \"so nguyen < "<<n<<"\": ";
+	cin>>banRo;
+	while(banRo>=n) {
+		cout<<"Nhap lai ban ro: ",cin>>banRo;
+	}
+
+	ll banMa = Mu(banRo, e, n);
+	cout<<"\nBan ma la: "<<banMa<<"\n\n";
+
+	cout<<"Ban ro sau khi giai ma la: "<<Mu(banMa, d, n)<<'\n';
+
 }
